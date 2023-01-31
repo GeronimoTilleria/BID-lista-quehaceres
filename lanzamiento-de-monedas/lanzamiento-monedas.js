7df1a63361c36bea91d3df1f21fcd8dd116bad1c
GeronimoTilleria <116458945+GeronimoTilleria@users.noreply.github.com>
2023-01-30 23:56:14 -0300

    Se agrego en la promesa un caso de error en la funcion de lanzamiento de monedas

--- lanzamiento-de-monedas/lanzamiento-monedas.js
+++ lanzamiento-de-monedas/lanzamiento-monedas.js
@@ -21,7 +21,23 @@ function fiveHeadsSync() {
 function fiveHeads() {
     return new Promise((resolve, reject) => {
         // tu código aquí!
-        resolve(fiveHeadsSync());
+        let headsCount = 0;
+        let attempts = 0;
+        while (headsCount < 5) {
+            attempts++;
+            let result = tossCoin();
+            console.log(`${result} was flipped`);
+            if (result === "heads") {
+                headsCount++;
+            } else {
+                headsCount = 0;
+            }
+        }
+        if(attempts <= 100) 
+            resolve(`It took ${attempts} tries to flip five "heads"`);
+        else
+            reject('Error: ha superado los 100 intentos');
+        
     });
 }
 
